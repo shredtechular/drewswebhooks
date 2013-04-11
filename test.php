0f7566ff9b3d9cad6e55d3c478e9577b2e19d1c5
root <root@ip-10-100-55-11.(none)>
2013-04-11 14:45:38 +0000

    updates

--- test.php
+++ test.php
@@ -1,11 +1,13 @@
 <?php 
 
-echo (exec('whoami'));
+//echo (exec('whoami'));
+
+$whoami = "you";
 
 exec('git pull'); 
 
-$file = fopen( 'testfile.txt', 'w+' );
-fwrite($file, 'hey<br/>');
+$file = fopen( 'testfile.txt', 'a+' );
+fwrite($file, 'hey'.$whoami.PHP_EOL.PHP_EOL);
 fclose( $file );
 
 echo "hey";?>
